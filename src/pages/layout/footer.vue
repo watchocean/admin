8f315445e6306ae1839a73179933cf437730ac91
alex <zhan572637307@gmail.com>
2018-10-17 15:01:39 +0800

    create project

---
+++ src/pages/layout/footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="main-footer">
+    Copyright &copy; 2017-2018 <a href="https://github.com/watchocean">Alex</a>. All rights reserved.
+  </footer>
+</template>
+<script>
+  export default {
+  }
+</script>
+<style>
+  .main-footer {
+    -webkit-transition: -webkit-transform 0.3s ease-in-out, margin 0.3s ease-in-out;
+    -moz-transition: -moz-transform 0.3s ease-in-out, margin 0.3s ease-in-out;
+    -o-transition: -o-transform 0.3s ease-in-out, margin 0.3s ease-in-out;
+    transition: transform 0.3s ease-in-out, margin 0.3s ease-in-out;
+    text-align: center;
+    padding: 5px 15px;
+    color: #444;
+    width: 100%;
+  }
+
+</style>
